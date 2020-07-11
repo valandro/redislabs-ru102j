68fc2a2a20916ce1a3eea5644275f4c1409479c7
Lucas Valandro da Rocha <lucas.rocha@realizecfi.com.br>
2020-07-11 14:31:20 -0300

    Add seventh programming challenge.

--- src/main/java/com/redislabs/university/RU102J/dao/RateLimiterSlidingDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/RateLimiterSlidingDaoRedisImpl.java
@@ -1,6 +1,13 @@
 package com.redislabs.university.RU102J.dao;
 
+import com.redislabs.university.RU102J.core.KeyHelper;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Pipeline;
+import redis.clients.jedis.Response;
+
+import java.time.ZonedDateTime;
+import java.util.Random;
 
 public class RateLimiterSlidingDaoRedisImpl implements RateLimiter {
 
@@ -19,6 +26,29 @@ public class RateLimiterSlidingDaoRedisImpl implements RateLimiter {
     @Override
     public void hit(String name) throws RateLimitExceededException {
         // START CHALLENGE #7
+        try (Jedis jedis = jedisPool.getResource()) {
+            String key = getKey(name);
+            Pipeline pipeline = jedis.pipelined();
+            Long timeStamp = System.currentTimeMillis();
+            String member = timeStamp + "-" + new Random().nextLong();
+            pipeline.zadd(key, timeStamp, member);
+            pipeline.zremrangeByScore(key, 0, timeStamp - windowSizeMS);
+            Response<Long> hits= pipeline.zcard(key);
+            pipeline.sync();
+
+            if (hits.get() > maxHits) {
+                throw new RateLimitExceededException();
+            }
+        }
+
         // END CHALLENGE #7
+    }
+
+    // [limiter]:[windowSize]:[name]:[maxHits]
+    private String getKey(String name) {
+        return KeyHelper.getKey("limiter:" +
+                windowSizeMS + ":" +
+                String.valueOf(name) + ":" +
+                String.valueOf(maxHits));
     }
 }
